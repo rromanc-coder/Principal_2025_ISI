378044f61593a814b185a0db291fe669a4220e9c
rromanc-coder <rrcuevas.uaemex@gmail.com>
2025-10-15 01:11:09 -0600

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserCreate(BaseModel):
     email: EmailStr
     full_name: str | None = None
     password: str
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    full_name: str | None = None
-    class Config:
-        from_attributes = True
-
 class LoginIn(BaseModel):
     email: EmailStr
     password: str
+
+class UserOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    email: EmailStr
+    full_name: str | None = None
