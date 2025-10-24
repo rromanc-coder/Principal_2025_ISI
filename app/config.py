74bac3e97b6d7e59c9755979a06f1c150dbdd22c
rromanc-coder <rrcuevas.uaemex@gmail.com>
2025-10-24 16:07:39 -0600

    Add configuration settings in config.py

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    APP_TITLE: str = "principal-isi"
+    APP_VERSION: str = "1.5.0"
+
+    # Monitor & logos
+    WG_HOST: str = "localhost"
+    TEAMS_JSON: str = "[]"
+    LOGO_UAEMEX_URL: str = ""
+    LOGO_ING_URL: str = ""
+
+    BASE_DIR: Path = Path(__file__).resolve().parent
+    STATIC_DIR: Path = BASE_DIR / "static"
+
+    class Config:
+        case_sensitive = False
+
+settings = Settings()
