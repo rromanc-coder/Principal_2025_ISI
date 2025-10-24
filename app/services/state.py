04321b218b8515cb940e483307f1de1850ee91af
rromanc-coder <rrcuevas.uaemex@gmail.com>
2025-10-24 16:36:29 -0600

    Update state.py

--- app/services/state.py
+++ app/services/state.py
@@ -2,8 +2,7 @@ import time
 from collections import defaultdict, deque
 
 HISTORY_WINDOW = 60  # muestras en memoria (cada /status)
-# name -> deque[{ts, up, lat, err}]
-history = defaultdict(lambda: deque(maxlen=HISTORY_WINDOW))
+history = defaultdict(lambda: deque(maxlen=HISTORY_WINDOW))  # name -> deque[{ts, up, lat, err}]
 last_error = {}  # name -> str
 
 def now_ts() -> float:
